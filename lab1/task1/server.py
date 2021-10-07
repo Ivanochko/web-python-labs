f41ef05888ab1750e1853a92ebde17b4a7e7f7fa
Ivanochko <vasa.ivanochko.17@gmail.com>
2021-10-08 01:58:07 +0300

    task1: repair datetime

--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import socket
-from datetime import date
+from datetime import datetime, date, time
 
 host = '127.0.0.1'
 port = 9999
@@ -18,12 +18,11 @@ while i < 3:
     print('Server is waiting!\n')
 
     clientsocket, addr = serversocket.accept()
-    print('Got a connection from {}'.format(addr))
 
     clientsocket.send('Input message?'.encode('utf-8'))
 
     client_message = clientsocket.recv(1024).decode('utf-8')
-    current_time = date.today().strftime("%A, %d. %B %Y %I:%M%p")
+    current_time = datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
 
     print('[' + current_time + '] :' + client_message)
     clientsocket.close()
